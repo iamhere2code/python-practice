3de90dcf1603ef88f61b642884a1bc702c15d9f1
alishaf18 <alishafaraz@gmail.com>
2025-05-24 18:22:50 -0700

    code from first python class

---
+++ test.py
@@ -0,0 +1,17 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print(f"{first_name} {last_name}")
+
+
+
+'''# Traditional Arithmatic Functions
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Python-specific
+print(a % b) # Remainder
+print(a // b) # Division result w/o remainder
+
+print(int(a + b))'''
